7ff2c26f2d28827aab91d615b6eac8424baaca5a
dnlslcd <danielsalcedoq@gmail.com>
2024-05-16 21:04:50 +0200

    "errores comunes" agregado

--- app.js
+++ app.js
@@ -14,3 +14,17 @@ button.addEventListener('click', async function(){
     // 3. modificar el DOM para mostrar el chiste al usuario
     jokeDIV.textContent = data.value;
 });
+
+
+// Errores comunes
+
+// 1. No poner bien la URL de la APIs de la cual quieres recibir la información
+//  await fetch("https://api.chucknorris.io/jokes/random");
+
+// 2. No saber interpretar el tipo de dato que os viene de la API
+// La forma más fácil de interpertar la estructura de datos que te devuelve la API-->
+// 1. console.log(data) --> Mostrar por consola los datos de la API antes de hacer nada
+// 2. En la página Web de la API, existe documentación con los campos y estructura de datos que devuelve la API
+
+// 3. No usar el ejercicio de chuck norris como base. Esta bien mirar como se hace el fetch en otros ejercicios. 
+//    No copiar y pegar, escribirlo todo para que se vaya quedando e interorizando
